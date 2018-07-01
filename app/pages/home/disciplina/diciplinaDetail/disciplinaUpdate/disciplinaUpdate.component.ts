bded78fddab320ee13bbd88c6eb175a72eedcb4e
Thiago Santiago <thiago.vps20@gmail.com>
2018-07-01 10:12:20 -0300

    add update screen

---
+++ app/pages/home/disciplina/diciplinaDetail/disciplinaUpdate/disciplinaUpdate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'disciplina-Update',
+    moduleId: module.id,
+    templateUrl: './disciplinaUpdate.component.html',
+    styleUrls: []
+})
+export class DisciplinaUpdateComponent {
+    
+}
