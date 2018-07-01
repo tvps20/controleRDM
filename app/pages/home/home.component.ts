1a59d240097b63ef69971268f5aee72316601347
Thiago Santiago <thiago.vps20@gmail.com>
2018-07-01 12:23:54 -0300

    Button of adding Corrected

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { Page } from 'ui/page';
 import { DataBaseService } from '~/services/database.service';
 import { Disciplina } from '~/shared/models/disciplina.model';
 
+
 @Component({
     selector: 'home',
     moduleId: module.id,
@@ -19,6 +20,7 @@ export class HomeComponent implements OnInit {
     public constructor(private databaseService: DataBaseService, private page: Page){
         this.setIcons();
         this.loadDisciplinas();
+
     }
 
     ngOnInit(): void { 
