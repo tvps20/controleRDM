033fe16c25592941283caae13c2f98e96152df00
Thiago Santiago <thiago.vps20@gmail.com>
2018-07-03 16:19:52 -0300

    Add checkBox

--- app/pages/home/disciplina/diciplinaDetail/disciplinaUpdate/disciplinaUpdate.component.ts
+++ app/pages/home/disciplina/diciplinaDetail/disciplinaUpdate/disciplinaUpdate.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core'
+import { Component, ViewChild, ElementRef } from '@angular/core'
+import { Disciplina } from '~/shared/models/disciplina.model';
 
 @Component({
     selector: 'disciplina-Update',
     moduleId: module.id,
     templateUrl: './disciplinaUpdate.component.html',
-    styleUrls: []
+    styleUrls: ['./disciplinaUpdate.component.css']
 })
 export class DisciplinaUpdateComponent {
+        @ViewChild("CB1") FirstCheckBox: ElementRef;
+        public disciplina: Disciplina;
     
+    public constructor() {
+        this.disciplina = new Disciplina('', undefined);
+    }
 }
