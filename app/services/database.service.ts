4a9e815171144b7206bbca9374766fe5e330f552
Thiago Santiago <thiago.vps20@gmail.com>
2018-07-20 13:30:54 -0300

    New function and new model horaExtra

--- app/services/database.service.ts
+++ app/services/database.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Disciplina } from "~/shared/models/disciplina.model";
 import { Horario } from "~/shared/models/horario.model";
-import { resolveDefinition } from "../../node_modules/@angular/core/src/view/util";
+import { HoraExtra } from "~/shared/models/horaExtra.model"
 
 var Sqlite = require("nativescript-sqlite");
 
@@ -13,6 +13,7 @@ export class DataBaseService {
             return (new Sqlite("controleCRE.db")).then(db => {
                 db.execSQL("CREATE TABLE IF NOT EXISTS disciplinas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, professor TEXT, cargaHoraria INTEGER, isClosed NUMERIC, status STRING, primeiraNota REAL, segundaNota REAL, terceiraNota REAL, quartaNota REAL, notaFinal REAL)").then(id => {
                     db.execSQL("CREATE TABLE IF NOT EXISTS horarios (id INTEGER PRIMARY KEY AUTOINCREMENT, sala TEXT, dia TEXT, hora TEXT, qtdAulas INTEGER, idDisciplina INTEGER, nomeDisciplina TEXT)")
+                    db.execSQL("CREATE TABLE IF NOT EXISTS horasExtra (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, cargaHoraria INTEGER)");
                     resolve(db);
                 }, error => {
                     console.log("[DATABASE] - ERROR CREATE TABLE!", error);
@@ -180,7 +181,7 @@ export class DataBaseService {
                     }
                     resolve(results);
                 }, error => {
-                    console.log("[DATABASE] - SELECT ERROR!");
+                    console.log("[DATABASE] - SELECT horario ERROR!");
                     reject(error);
                 });
             })              
@@ -207,77 +208,65 @@ export class DataBaseService {
                     }
                     resolve(results);
                 }, error => {
-                    console.log("[DATABASE] - SELECT ERROR!");
+                    console.log("[DATABASE] - SELECT horario ERROR!");
                     reject(error);
                 });
             })              
         });
     }
+
+
+    // Rotinas para horas Extras
+
+    public insertHoraExtra(horaExtra: HoraExtra){
+        return new Promise((resolve, reject) => {
+            this.createDB().then((res: any) => {
+                res.execSQL("INSERT INTO horasExtra (nome, cargaHoraria) VALUES (?, ?)", [horaExtra.nome, horaExtra.cargaHoraria]).then( id => {
+                    console.log("INSERT horasExtra RESULT: ", id);
+                    resolve(true);
+                }, error => {
+                    console.log("[DATABASE] - INSERT horasExtra FAILED!", error);
+                    reject(false);
+                })
+            })
+        })
+    }
+
+    // Retorna um objeto do tipo Horario
+    public getAllHorasExtra(){
+        return new Promise((resolve, reject) => {
+            this.createDB().then((res: any) => {
+                return res.all("SELECT * FROM horasExtra ").then(rows => {
+                    let results: HoraExtra[] = [];
+                    for(let row in rows){
+                        let result: HoraExtra = new HoraExtra();
+
+                        result.id = rows[row][0];
+                        result.nome = rows[row][1];
+                        result.cargaHoraria = rows[row][2];
+
+                        results.push(result);
+                    }
+                    resolve(results);
+                }, error => {
+                    console.log("[DATABASE] - SELECT horasExtra ERROR!");
+                    reject(error);
+                });
+            })              
+        });
+    }
+
+    public deleteHoraExtra(id: number){
+        return new Promise((resolve, reject) => {
+            this.createDB().then((res: any) => {
+                res.execSQL("DELETE FROM horasExtra WHERE id=" + id).then(id => {
+                    console.log("DELETE horasExtra RESULT: ", id);
+                    resolve(true);
+                }, error => {
+                    console.log("[DATABASE] - DELETE horasExtra FAILED!", error);
+                    reject(false);
+                })
+            })
+        })
+    }
 }
-
-// TODO: Não mostra os dados quando inicia, so depois de adicionar...
-// import { Injectable } from "@angular/core";
-// import { Disciplina } from "~/shared/models/disciplina.model";
-
-// var Sqlite = require("nativescript-sqlite");
-
-// @Injectable()
-// export class DataBaseService {
-
-//     private database: any;
-
-//     public constructor(){
-//         (new Sqlite("controleCRE.db")).then(db => {
-//             db.execSQL("CREATE TABLE IF NOT EXISTS disciplinas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, professor TEXT, cargaHoraria INTEGER, horario TEXT, sala TEXT, isClosed NUMERIC, status STRING, primeiraNota REAL, segundaNota REAL, terceiraNota REAL, quartaNota REAL, notaFinal REAL)").then(id => {
-//                 this.database = db;
-//             }, error => {
-//                 console.log("[DATABASE] - ERROR CREATE TABLE!", error);
-//             });
-//         }, error => {
-//             console.log("[DATABASE] - ERROR OPEM DB!", error);
-//         });  
-//     }
-
-
-//     public insert(disciplina: Disciplina){
-//         return new Promise((resolve, reject) => {
-//             this.database.execSQL("INSERT INTO disciplinas (nome, professor) VALUES (?,?)", [disciplina.nome, disciplina.professor]).then(id => {
-//                 console.log("INSERT RESULT: ", id);
-//                 resolve(true);
-//             }, error => {
-//                 console.log("[DATABASE] - INSERT FAILED!", error);
-//                 reject(false);
-//             });
-//         });
-
-//     }
-
-//     public getAll(){
-//         return new Promise((resolve, reject) => {
-//             this.database.all("SELECT * FROM disciplinas").then(rows => {
-//                 let result: any[] = [];
-//                 for(let row in rows){
-//                     result.push({
-//                         "id": rows[row][0],
-//                         "nome": rows[row][1],
-//                         "professor": rows[row][2],
-//                         "cargaHoraria": rows[row][3],
-//                         "horario": rows[row][4],
-//                         "sala": rows[row][5],
-//                         "isClosed": rows[row][6],
-//                         "status": rows[row][7],
-//                         "primeiraNota": rows[row][8],
-//                         "segundaNota": rows[row][9],
-//                         "terceiraNota": rows[row][10],
-//                         "quartaNota": rows[row][11],
-//                         "notaFinal": rows[row][12]
-//                     });
-//                 }
-//                 resolve(result);
-//             }, error => {
-//                 console.log("[DATABASE] - SELECT ERROR!");
-//                 reject(error);
-//             });
-//         })              
-//     }
-// }
---
+++ app/shared/models/horaExtra.model.ts
@@ -0,0 +1,5 @@
+export class HoraExtra {
+    public id: number;
+    public nome: string;
+    public cargaHoraria: number;
+}
