92133652bb63d29b55b39c30af369b0cd444467d
Thiago Santiago <thiago.vps20@gmail.com>
2018-08-17 10:43:00 -0300

    Correting images and horario style

--- app/shared/models/horario.model.ts
+++ app/shared/models/horario.model.ts
@@ -15,10 +15,23 @@ export class Horario {
     public makeHorario(){
         let horario:string;
         if(this.qtdAulas <= 1)
-            horario = this.dia + "/" + this.qtdAulas + " aula" + " " + this.hora + "h" + "/" + this.sala;
+            return horario = this.dia + "/" + this.qtdAulas + " aula" + " " + this.hora + "h" + "/" + this.sala;
         else
-            horario = this.dia + "/" + this.qtdAulas + " aulas" + " " + this.hora + "h" + "/" + this.sala;
+            return horario = this.dia + "/" + this.qtdAulas + " aulas" + " " + this.hora + "h" + "/" + this.sala;
+    }
 
-        return horario;
+    public getHorario(){
+        if(this.qtdAulas <= 1)
+            return `Hórario: ${this.hora} / ${this.qtdAulas} aula`;
+        else
+            return `Hórario: ${this.hora} / ${this.qtdAulas} aulas`;
+    }
+
+    public getSala(){
+        return `Sala: ${this.sala}`;
+    }
+
+    public getNomeDisciplina(){
+        return `Disciplina: ${this.nomeDisciplina}`;
     }
 }
